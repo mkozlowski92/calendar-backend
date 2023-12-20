e28214cf69ae7639fb417cf42911bebe5f759977
Marcin Kozłowski <99545322+mkozlowski92@users.noreply.github.com>
2023-12-20 20:09:38 +0100

    added base Calendar and User models

---
+++ src/main/java/com/example/calendarbackend/model/Calendar.java
@@ -0,0 +1,30 @@
+package com.example.calendarbackend.model;
+
+import java.time.LocalDate;
+
+public class Calendar {
+    private long id;
+
+    private long userId;
+
+    private LocalDate date;
+
+    private boolean period;
+
+    private boolean moodType1;
+
+    private boolean moodType2;
+
+    private boolean moodType3;
+
+    private boolean moodType4;
+
+    private boolean moodType5;
+
+    private boolean moodType6;
+
+    private boolean moodType7;
+
+    private String notes;
+
+}
---
+++ src/main/java/com/example/calendarbackend/model/User.java
@@ -0,0 +1,12 @@
+package com.example.calendarbackend.model;
+
+public class User {
+    private long id;
+
+    private String userName;
+
+    private String passwordHashed;
+
+    private long userIdConnectedAccount;
+
+}
