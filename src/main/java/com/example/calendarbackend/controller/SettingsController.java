1215398cb220fccc46943dac23cd0e8c7021ff71
Marcin Kozłowski <99545322+mkozlowski92@users.noreply.github.com>
2024-01-16 23:37:50 +0100

    created controllers

---
+++ src/main/java/com/example/calendarbackend/controller/SettingsController.java
@@ -0,0 +1,9 @@
+package com.example.calendarbackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/settings")
+public class SettingsController {
+}
