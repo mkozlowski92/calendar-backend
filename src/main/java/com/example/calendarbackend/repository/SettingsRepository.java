203c6d0973156ab56cfb3b3ab9259a045644d84c
Marcin Kozłowski <99545322+mkozlowski92@users.noreply.github.com>
2024-01-17 13:57:25 +0100

    added JAVADOC annotations for repositories

--- src/main/java/com/example/calendarbackend/repository/SettingsRepository.java
+++ src/main/java/com/example/calendarbackend/repository/SettingsRepository.java
@@ -4,6 +4,9 @@ import com.example.calendarbackend.model.Settings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for managing settings of users in the database.
+ */
 @Repository
 public interface SettingsRepository extends JpaRepository<Settings, Long> {
 }
--- src/main/java/com/example/calendarbackend/repository/UserRepository.java
+++ src/main/java/com/example/calendarbackend/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.example.calendarbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for managing users in the database.
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
