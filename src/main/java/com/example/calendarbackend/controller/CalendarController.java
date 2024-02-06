19633c2e652b2fc36d7d637a957cc4f197dd46fa
Marcin Kozłowski <99545322+mkozlowski92@users.noreply.github.com>
2024-02-06 19:09:32 +0100

    Get calendar (#9)
    * added getCalendar method to CalendarService
    
    * fixed updateCalendar Swagger UI annotation
    created getUserOrPartnerId method (2 usages)
    
    * returns List<Calendar> and passed manual tests
    
    * cleaned updateCalendar, getCalendar
    changed method name to getMainAccountUserId
    
    * updated SwaggerUI and JAVADOC annotations

--- src/main/java/com/example/calendarbackend/controller/CalendarController.java
+++ src/main/java/com/example/calendarbackend/controller/CalendarController.java
@@ -4,7 +4,6 @@ import com.example.calendarbackend.exception.AccountDoesNotExist;
 import com.example.calendarbackend.exception.MainAccountIsNotConnected;
 import com.example.calendarbackend.exception.PeriodSameStartAndEnd;
 import com.example.calendarbackend.model.Calendar;
-import com.example.calendarbackend.model.Settings;
 import com.example.calendarbackend.service.CalendarService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
@@ -48,7 +47,7 @@ public class CalendarController {
      * @throws MainAccountIsNotConnected - Main account isn't connected to this partner account.
      * @throws PeriodSameStartAndEnd - Can't have start and end of period in the same day.
      */
-    @Operation(summary = "Get settings")
+    @Operation(summary = "Update calendar")
     @ApiResponses(
             value = {
                     @ApiResponse(
@@ -80,10 +79,42 @@ public class CalendarController {
         }
     }
 
+    /**
+     * Gets list of calendars days for whole month.
+     * @param userId - user ID.
+     * @param year - year.
+     * @param month - month.
+     * @return List of calendar days.
+     * @throws AccountDoesNotExist - Account with this ID doesn't exist.
+     * @throws MainAccountIsNotConnected - Main account isn't connected to this partner account.
+     */
+    @Operation(summary = "Get calendar for whole month")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Got list of days.",
+                            content = @Content(
+                                    mediaType="application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Calendar.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Main account isn't connected to this partner account.",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Server error",
+                            content = @Content
+                    )
+            }
+    )
     @GetMapping("/getCalendar")
-    private ResponseEntity<List<Calendar>> getCalendar() {
+    private ResponseEntity<List<Calendar>> getCalendar(@RequestParam Long userId, @RequestParam Long year, @RequestParam Long month) throws AccountDoesNotExist, MainAccountIsNotConnected {
         try {
-            return null;
+            return ResponseEntity.ok(calendarService.getCalendar(userId, year, month));
         } catch (DataAccessException exception){
             return ResponseEntity.internalServerError().build();
         }
--- src/main/java/com/example/calendarbackend/service/CalendarService.java
+++ src/main/java/com/example/calendarbackend/service/CalendarService.java
@@ -9,6 +9,8 @@ import com.example.calendarbackend.repository.SettingsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CalendarService {
 
@@ -52,19 +54,48 @@ public class CalendarService {
      */
     public Calendar updateCalendar(Long userId, Calendar calendar) throws AccountDoesNotExist, MainAccountIsNotConnected, PeriodSameStartAndEnd {
 
-        if (!userService.isMainAccount(userId)) {
-            if (settingsRepository.findByPartnerUserId(userId)!=null)
-                userId = settingsRepository.findByPartnerUserId(userId).getUser().getId();
-            else throw new MainAccountIsNotConnected();
-        }
+        Long mainUserId = getMainAccountUserId(userId);
 
         if (calendar.isPeriodStart() && calendar.isPeriodEnd()) throw  new PeriodSameStartAndEnd();
 
-        if (calendarRepository.findCalendarByUserIdAndDate(userId, calendar.getDate())!=null)
-            calendar.setId(calendarRepository.findCalendarByUserIdAndDate(userId, calendar.getDate()).getId());
-        calendar.setUser(userService.getUser(userId));
+        if (calendarRepository.findCalendarByUserIdAndDate(mainUserId, calendar.getDate())!=null)
+            calendar.setId(calendarRepository.findCalendarByUserIdAndDate(mainUserId, calendar.getDate()).getId());
+        calendar.setUser(userService.getUser(mainUserId));
 
         return calendarRepository.save(calendar);
+    }
+
+    /**
+     * Gets list of calendars days for whole month.
+     * @param userId - user ID.
+     * @param year - year.
+     * @param month - month.
+     * @return List of calendar days.
+     * @throws AccountDoesNotExist - Account with this ID doesn't exist.
+     * @throws MainAccountIsNotConnected - Main account isn't connected to this partner account.
+     */
+    public List<Calendar> getCalendar(Long userId, Long year, Long month) throws AccountDoesNotExist, MainAccountIsNotConnected {
+
+        Long mainUserId = getMainAccountUserId(userId);
+
+        return calendarRepository.findAllByUserId(mainUserId).stream().filter(c->c.getDate().getYear()==year&&c.getDate().getMonthValue()==month).toList();
+
+    }
+
+    /**
+     * Gets ID of Main Account for Partner Account
+     * @param userId - user ID.
+     * @return ID of Main account.
+     * @throws AccountDoesNotExist - Account with this ID doesn't exist.
+     * @throws MainAccountIsNotConnected - Main account isn't connected to this partner account.
+     */
+    private Long getMainAccountUserId(Long userId) throws AccountDoesNotExist, MainAccountIsNotConnected {
+        if (!userService.isMainAccount(userId)) {
+            if (settingsRepository.findByPartnerUserId(userId) != null)
+                userId = settingsRepository.findByPartnerUserId(userId).getUser().getId();
+            else throw new MainAccountIsNotConnected();
+        }
+        return userId;
     }
 
 }
